86654d2817a55905d7f36963432893fdbe56259e
Kalingga Dwindra Putraka <kalingga221@gmail.com>
2025-01-13 08:42:56 +0800

    regis login

--- masuk.php
+++ masuk.php
@@ -14,19 +14,13 @@
 <body>
     <form action="" class="form">
         <header>
-            <img src="assets/img/daftar.svg" alt="">
-            <h1>Daftar</h1>
+            <img src="assets/img/masuk.svg" alt="">
+            <h1>Masuk</h1>
         </header>
-        <input type="text" name="" id="" placeholder="Nama Lengkap">
-        <input type="number" name="" id="" placeholder="NIM">
-        <select name="fakultas" id="fakultas">
-            <option value="" disabled selected>Fakultas</option>
-            <option value="Informatika">Informatika</option>
-        </select>
         <input type="text" name="" id="" placeholder="Username">
         <input type="password" name="" id="" placeholder="Password">
         <input type="submit" value="Daftar">
-        <p>Anda sudah punya akun? <a href="masuk.php">Masuk disini</a></p>
+        <p>Belum punya akun? <a href="index.php">Daftar disini</a></p>
     </form>
     <img src="assets/img/reglog.png" alt="pic of portal" class="side-pic">
 
